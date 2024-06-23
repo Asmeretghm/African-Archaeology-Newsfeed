ce8e151fe31bb49e145ca643499d46842d7114c3
asmeretghm <asmeretghm@gmail.com>
2024-06-23 11:39:15 -0400

    added readme file

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # African-Archaeology-Newsfeed
 This is newsfeed for African archaeology.
+
+To access this project, it has be run using live-server. I used the following information to produce this newsfeed. 
+
+API used: https://newsapi.org
+
+
+
+
---
+++ script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const newsList = document.getElementById('news-list');
+
+    // Function to fetch news data from an API (simulated data here)
+    async function fetchNews() {
+        try {
+            const response = await fetch('https://newsapi.org/v2/everything?q=african%20archaeology&apiKey=df51ce3506e04159a66d77ea59bbe61f');
+            const data = await response.json();
+            console.log(data)
+            return data.articles;
+        } catch (error) {
+            console.error('Error fetching news:', error);
+            return [];
+        }
+    }
+
+    // Function to populate news feed
+    async function populateNewsFeed() {
+        const newsData = await fetchNews();
+
+        newsList.innerHTML = ' ';
+        newsData.forEach(item => {
+            const li = document.createElement('li');
+            li.className = 'news-item';
+            li.innerHTML = `
+                <h2 class="news-title">${item.title}</h2>
+                <img src=${item.urlToImage}>            
+                <p class="news-description">${item.description}</p>
+                <a class="news-link" href="${item.url}" target="_blank">Read more</a>
+
+                            `;
+            newsList.appendChild(li);
+        });
+    }
+    setInterval(populateNewsFeed, 2000)
+    // Initial population of news feed
+    //populateNewsFeed();
+});
+
+
+
